14fd38fa0d5026e84f022b20c18f7532593f87e7
Kirsten <kirsten.grahl@outlook.de>
2021-11-07 16:24:40 +0100

    useState für Benutzereingaben

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, TextInput } from 'react-native';
 import { Button } from 'react-native-elements';
 import {loginContainer, loginInputContainer} from '../constants/LayoutStyles';
 
 export default LoginScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const usernameInputHandler = enteredText => {
+    setUsername(enteredText);
+  };
+
+  const passwordInputHandler = enteredText => {
+    setPassword(enteredText);
+  };
 
   const pressHandler = () => {
     navigation.navigate('Home');
@@ -12,8 +22,17 @@ export default LoginScreen = ({ navigation }) => {
   return (
     <View style = {loginContainer()}>
         <Text>Login</Text>
-        <TextInput style = {loginInputContainer()} placeholder="USER"/>
-        <TextInput style = {loginInputContainer()} placeholder="PASSWORD"/>
+        <TextInput 
+          style = {loginInputContainer()}
+          placeholder = "USER"
+          onChangeText = {usernameInputHandler}
+        />
+        <TextInput 
+          style = {loginInputContainer()} 
+          placeholder = "PASSWORD" 
+          secureTextEntry = {true} 
+          onChangeText = {passwordInputHandler}
+        />
         <Button title="Login" onPress={pressHandler}/>
     </View>
   );
